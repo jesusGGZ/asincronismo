d76ed2f6951d4f1da745c5f2a33a674338b63982
jesusGGZ <j.gr.zurita@gmail.com>
2021-02-22 11:53:00 -0600

    Implementando promesas

--- src/promise/index.js
+++ src/promise/index.js
@@ -12,13 +12,27 @@ somethingWillHappen()
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
-const somethingWillHappen2 = () => (
+const somethingWillHappen2 = () => {
     return new Promise((resolve, reject) => {
         if (true) {
             setTimeout(() => {
-                
-                
-            }, timeout);
+                resolve('True');
+            }, 2000);
+        }else{
+            const error = new Error('Whoooops!');
+            reject(error);
         }
     });
-)
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Array of results', response);
+    })
+    .catch(err => {
+        colose.error(err);
+    })
